5f83ed2b2dd361b725323b17a2ccee9a12a64db5
Cheshulko <chehhmann.nick@gmail.com>
2018-12-28 11:06:44 +0200

    Add bottomNavigationBar

--- lib/instagramHome.dart
+++ lib/instagramHome.dart
@@ -17,10 +17,46 @@ class InstagramHome extends StatelessWidget {
     ],
   );
 
+  final bottomNavigationBar = new Container(
+    color: Colors.white,
+    height: 50.0,
+    alignment: Alignment.center,
+    child: new BottomAppBar(
+      child: new Row(
+        // alignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          new IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {},
+          ),
+          new IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null,
+          ),
+          new IconButton(
+            icon: Icon(Icons.add_box),
+            onPressed: null,
+          ),
+          new IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: null,
+          ),
+          new IconButton(
+            icon: Icon(Icons.account_box),
+            onPressed: null,
+          ),
+        ],
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: topBar,
+      body: new Container(),
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
 }
